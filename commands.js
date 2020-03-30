38324f74d66cce6f1cfa6886a18c18e3c5d1ae2c
codyzu <cody.zuschlag@gmail.com>
2020-03-30 21:57:15 +0200

    feat: 🎸 add video capture

--- commands.js
+++ commands.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const path = require('path')
+const tmp = require('tmp')
+const firebase = require('firebase')
 const shell = require('shelljs')
 const shutdown = require('./shutdown')
 const motor = require('./motor')
@@ -9,6 +12,7 @@ module.exports = {
   feed,
   console: consoleLog,
   wave,
+  recordVideo,
   exit,
   reboot: rebootSystem,
   shutdown: shutdownSystem,
@@ -65,4 +69,40 @@ async function shutdownSystem() {
       shell.exec('shutdown -h now')
     },
   }
+}
+
+async function recordVideo() {
+  log.debug('Record video command')
+  const {dirPath, cleanup} = await withTempDir()
+  try {
+    const fileName = `${new Date().toString()}.mp4`
+    const filePath = path.resolve(dirPath, fileName)
+    await shell.exec(
+      `ffmpeg -t 10 -f v4l2 -framerate 30 -video_size 1280x720 -c:v mjpeg -i /dev/video0 -f mp4 -vcodec h264_omx -r 30 -b:v 1M -maxrate 1M -bufsize 2M "${filePath}"`
+    )
+    await firebase
+      .storage()
+      .ref(`captures/${fileName}`)
+      .put('???')
+  } finally {
+    cleanup()
+  }
+}
+
+function withTempDir() {
+  return new Promise((resolve, reject) => {
+    tmp.dir(
+      {
+        unsafeCleanup: true,
+        prefix: 'capture_',
+      },
+      (err, dirPath, cleanup) => {
+        if (err) {
+          return reject(err)
+        }
+
+        resolve({dirPath, cleanup})
+      }
+    )
+  })
 }
---
+++ src/commands/camera.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const tmp = require('tmp')
+const {Storage} = require('@google-cloud/storage')
+const shell = require('shelljs')
+const log = require('../../logging')
+
+module.exports = {
+  recordVideo,
+}
+
+const storage = new Storage({
+  keyFilename: path.resolve(__dirname, '../../.logging-service-account.json'),
+})
+const bucket = storage.bucket('feeder-88e0a.appspot.com')
+
+recordVideo()
+
+async function recordVideo() {
+  const {dirPath, cleanup} = await withTempDir()
+  try {
+    const fileName = `${new Date().toString()}.mp4`
+    const filePath = path.resolve(dirPath, fileName)
+    log.debug(`Camera capturing video to: ${filePath}`)
+    await shell.exec(
+      `ffmpeg -t 10 -f v4l2 -framerate 30 -video_size 1280x720 -c:v mjpeg -i /dev/video0 -f mp4 -vcodec h264_omx -r 30 -b:v 1M -maxrate 1M -bufsize 2M "${filePath}"`
+    )
+
+    const storagePath = `videos/${fileName}`
+    log.debug(`Uploading to: ${storagePath}`)
+    await bucket.upload(filePath, {destination: storagePath})
+
+    // await firebase
+    //   .storage()
+    //   .ref(`captures/${fileName}`)
+    //   .put('???')
+  } finally {
+    cleanup()
+  }
+}
+
+function withTempDir() {
+  return new Promise((resolve, reject) => {
+    tmp.dir(
+      {
+        unsafeCleanup: true,
+        prefix: 'capture_',
+      },
+      (err, dirPath, cleanup) => {
+        if (err) {
+          return reject(err)
+        }
+
+        resolve({dirPath, cleanup})
+      }
+    )
+  })
+}
