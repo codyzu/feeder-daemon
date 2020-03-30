10842d07fe98f0f17ca8c1313a3d0d13bcf36313
codyzu <cody.zuschlag@gmail.com>
2020-03-30 23:06:29 +0200

    feat: 🎸 fix video capture and upload

--- src/commands/camera.js
+++ src/commands/camera.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const tmp = require('tmp')
-const {Storage} = require('@google-cloud/storage')
+const { Storage } = require('@google-cloud/storage')
 const shell = require('shelljs')
 const log = require('../../logging')
 
@@ -9,25 +9,25 @@ module.exports = {
 }
 
 const storage = new Storage({
-  keyFilename: path.resolve(__dirname, '../../.logging-service-account.json'),
+  keyFilename: path.resolve(__dirname, '../../logging-service-account.json'),
 })
 const bucket = storage.bucket('feeder-88e0a.appspot.com')
 
 recordVideo()
 
 async function recordVideo() {
-  const {dirPath, cleanup} = await withTempDir()
+  const { dirPath, cleanup } = await withTempDir()
   try {
-    const fileName = `${new Date().toString()}.mp4`
+    const fileName = `${new Date().toISOString()}.mp4`
     const filePath = path.resolve(dirPath, fileName)
     log.debug(`Camera capturing video to: ${filePath}`)
     await shell.exec(
-      `ffmpeg -t 10 -f v4l2 -framerate 30 -video_size 1280x720 -c:v mjpeg -i /dev/video0 -f mp4 -vcodec h264_omx -r 30 -b:v 1M -maxrate 1M -bufsize 2M "${filePath}"`
+      `ffmpeg -t 10 -f v4l2 -framerate 30 -video_size 1280x720 -c:v mjpeg -i /dev/video0 -f mp4 -vcodec h264_omx -r 30 -b:v 2M -maxrate 2M -bufsize 3M "${filePath}"`
     )
 
     const storagePath = `videos/${fileName}`
     log.debug(`Uploading to: ${storagePath}`)
-    await bucket.upload(filePath, {destination: storagePath})
+    await bucket.upload(filePath, { destination: storagePath })
 
     // await firebase
     //   .storage()
@@ -50,7 +50,7 @@ function withTempDir() {
           return reject(err)
         }
 
-        resolve({dirPath, cleanup})
+        resolve({ dirPath, cleanup })
       }
     )
   })
